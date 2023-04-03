27a83934f13c93813f419b992df6d5d210ef844c
유영빈 <97148877+BTSBRINGMEHERE@users.noreply.github.com>
2023-04-03 21:19:15 +0900

    image optimization
    이미지 사이즈 최적화
    object-fit:cover

--- next-port/components/NewsArticleEntry.tsx
+++ next-port/components/NewsArticleEntry.tsx
@@ -2,6 +2,7 @@ import { NewsArticle } from "@/models/NewsArticles"
 import Image from "next/image"
 import { Card } from "react-bootstrap"
 import placeholderImage from "@/assets/images/newsarticle_placeholder.jpg"
+import styles from "@/styles/NewsArticleEntry.module.css"
 
 interface NewsArticleEntryProps {
 	article: NewsArticle
@@ -13,7 +14,13 @@ const NewsArticleEntry = ({ article: { title, description, url, urlToImage } }: 
 	return (
 		<a href={url}>
 			<Card className="h-100">
-				<Image src={validImageUrl || placeholderImage} width={500} height={200} alt="News article image" />
+				<Image
+					src={validImageUrl || placeholderImage}
+					width={500}
+					height={200}
+					alt="News article image"
+					className={`card-img-top ${styles.image}`}
+				/>
 
 				<Card.Body>
 					<Card.Title>{title}</Card.Title>
